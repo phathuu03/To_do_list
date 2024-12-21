0dd15ca10978b42642d6baaa3f1a6951d944e3ed
phatnhph <phatnhph40305@fpt.edu.vn>
2024-12-21 16:08:38 +0700

    updat

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,12 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
 }
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        val nav_version = "2.8.4"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+    }
+}
